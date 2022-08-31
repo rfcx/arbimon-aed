e85b58f224c90a856faabac03e04eb589ae38f79
jackgle <jglebien@gmail.com>
2022-08-31 10:11:13 -0600

    add SQS functions to conductor

--- functions/conductor/main.py
+++ functions/conductor/main.py
@@ -165,4 +165,48 @@ def driver(event, context):
     return {
         'status': 200,
     }
+
+
+def get_queue(resource, name):
+    """
+    Gets an SQS queue by name.
+
+    :param name: The name that was used to create the queue.
+    :return: A Queue object.
+    """
+    try:
+        queue = resource.get_queue_by_name(QueueName=name)
+        logger.info("Got queue '%s' with URL=%s", name, queue.url)
+    except ClientError as error:
+        logger.exception("Couldn't get queue named %s.", name)
+        raise error
+    else:
+        return queue
+
+
+def send_message(queue, message_body, message_attributes=None):
+    """
+    Send a message to an Amazon SQS queue.
+
+    Usage is shown in usage_demo at the end of this module.
+
+    :param queue: The queue that receives the message.
+    :param message_body: The body text of the message.
+    :param message_attributes: Custom attributes of the message. These are key-value
+                              pairs that can be whatever you want.
+    :return: The response from SQS that contains the assigned message ID.
+    """
+    if not message_attributes:
+        message_attributes = {}
+
+    try:
+        response = queue.send_message(
+            MessageBody=message_body,
+            MessageAttributes=message_attributes
+        )
+    except ClientError as error:
+        logger.exception("Send message failed: %s", message_body)
+        raise error
+    else:
+        return response
     
