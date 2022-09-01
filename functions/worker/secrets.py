ec6b1b4ccecfcc138d7c45a4f2d48a4cd9a37002
jackgle <jglebien@gmail.com>
2022-09-01 04:39:20 -0600

    remove region argument for aws secrets

--- functions/worker/secrets.py
+++ functions/worker/secrets.py
@@ -7,10 +7,8 @@ from botocore.exceptions import ClientError
 def aws_secrets(secret_name):
 
     session = boto3.session.Session()
-    region_name = "us-east-1"
     client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
+        service_name='secretsmanager'
     )
 
     try:
